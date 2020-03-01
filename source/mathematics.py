263eb8be052a1eb92a6404f88ccd9338cafc75fa
JekaBB <43681373+JekaBB@users.noreply.github.com>
2020-03-02 01:36:25 +0300

    A math module has been created. The is_pixel_black() function calculates whether a pixel should be painted over

---
+++ source/mathematics.py
@@ -0,0 +1,14 @@
+import math
+from decimal import Decimal, getcontext
+
+
+def is_pixel_black(x, y, k):
+    getcontext().prec = 10000
+    y += k
+
+    power_of_two = (-17 * math.floor(x)) - (math.floor(Decimal(y)) % 17)
+    formula = math.floor((math.floor(Decimal(y)/17) * (2 ** Decimal(power_of_two))) % 2)
+
+    if formula > 0.5:
+        return True
+    return False
