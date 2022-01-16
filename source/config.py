6c008f92ea59fe05bff02a915a2261de9eeb3388
HanSaloZu <conswintcomin@gmail.com>
2022-01-16 14:07:29 +0300

    Add config.py file with editable application settings
    By editing the config.py file, the user can change the screen size(SCREEN_HEIGHT, SCREEN_WIDTH),
    the pixel size for image scaling(PIXEL_SIZE), and the title of the application window(SCREEN_TITLE)

---
+++ source/config.py
@@ -0,0 +1,9 @@
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+SCREEN_TITLE = "Tupper's self-referential formula"
+SCREEN_HEIGHT = 420
+SCREEN_WIDTH = 1420
+
+PIXEL_SIZE = 10
