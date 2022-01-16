07770e6aa422aaff565924a5229cec967b5f357f
HanSaloZu <conswintcomin@gmail.com>
2022-01-16 15:15:34 +0300

    Refactor main.py
    Remove the decimal library import and all its usages. Import config.py. Replace all "magic numbers" with variables/constants

--- source/main.py
+++ source/main.py
@@ -1,36 +1,34 @@
-import os
 import formula
 import display
-from decimal import Decimal, getcontext
+import config as cfg
 
 
 if __name__ == "__main__":
-    screen = display.create_screen()
+    with open(cfg.BASE_DIR + "/source/k.txt", "r") as k_txt:
+        k = int(k_txt.read())
 
-    turtle = display.create_turtle()
-    turtle.up()
+    screen = display.create_screen(
+        cfg.SCREEN_TITLE, cfg.SCREEN_WIDTH, cfg.SCREEN_HEIGHT)
 
-    getcontext().prec = 10000
-    DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    _NUMBER_OF_BITMAP_COLUMNS = 106
+    _NUMBER_OF_BITMAP_ROWS = 17
+    START_X = -(_NUMBER_OF_BITMAP_COLUMNS * cfg.PIXEL_SIZE) / 2
+    START_Y = -(_NUMBER_OF_BITMAP_ROWS * cfg.PIXEL_SIZE) / 2
 
-    with open(DIR + "/source/k.txt", "r") as txt_k:
-        k = Decimal(txt_k.read())
+    x = START_X
+    y = START_Y
+    turtle = display.create_turtle(cfg.PIXEL_SIZE)
 
-    x = -550
-    y = -128
-
-    pixel_size = 12
-
-    for column in range(106):
-        for row in range(17):
+    for column in range(_NUMBER_OF_BITMAP_COLUMNS):
+        for row in range(_NUMBER_OF_BITMAP_ROWS):
             screen.update()
 
             if formula.is_pixel_black(column, row, k):
                 display.draw_pixel(turtle, x, y)
 
-            y += pixel_size
+            y += cfg.PIXEL_SIZE
 
-        y = -128
-        x += pixel_size
+        y = START_Y
+        x += cfg.PIXEL_SIZE
 
     screen.mainloop()
